98bff136ec4c088a86b55ea55c8106fb0ac8a6c7
Rockie Yang <eyouyan@gmail.com>
2017-08-23 13:26:47 +0200

    add oracle jdbc example

---
+++ src/main/scala/jdbc/Oracle.scala
@@ -0,0 +1,63 @@
+package jdbc
+
+import java.sql.{Connection, DriverManager, ResultSet}
+
+// Oracle is always tricky to handle for proprietary reason
+// We can not just simply using maven dependency!!!
+//
+// Steps 1: run a docker container to have simulation environment
+//
+//      docker run -d -p 8080:8080 -p 1521:1521 --name oracle sath89/oracle-12c
+//
+// Step 2: Copy driver jar files from container
+//
+//      docker cp oracle:/u01/app/oracle-product/12.1.0/xe/jdbc/lib ~/git/explore-scala/lib/
+//
+// Step 3: Modify pom file
+//
+//          <dependency>
+//              <groupId>com.oracle</groupId>
+//              <artifactId>ojdbc</artifactId>
+//              <version>14</version>
+//              <scope>system</scope>
+//              <systemPath>${basedir}/lib/ojdbc6.jar</systemPath>
+//          </dependency>
+//
+//
+object Oracle {
+  def main(args: Array[String]): Unit = {
+      var connection: Connection = null
+      try {
+        // make the connection
+        //step1 load the driver class
+        val driver = Class.forName("oracle.jdbc.driver.OracleDriver")
+
+        //step2 create  the connection object
+        connection = DriverManager.getConnection(
+          "jdbc:oracle:thin:@localhost:1521:xe","system","oracle");
+
+
+        // create the statement, and run the select query
+        val statement = connection.createStatement()
+
+        statement.execute("create table test(id number(10) not null, name varchar2(50) not null)")
+        statement.execute("insert into test(id, name) values(1, 'rockie')")
+        statement.execute("insert into test(id, name) values(2, 'yang')")
+
+        val resultSet: ResultSet = statement.executeQuery("SELECT id, name from test")
+        while ( resultSet.next() ) {
+          val id = resultSet.getString("id")
+          val name = resultSet.getString("name")
+          println(s"id = $id, name = $name")
+        }
+
+        statement.execute("drop table test")
+      } catch {
+        case e => e.printStackTrace
+      } finally {
+        connection.close()
+      }
+
+
+  }
+}
